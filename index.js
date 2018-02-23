069d21a0dadc0375708e062edf526ecc637cfeb6
David <david.joech@gmail.com>
2018-02-23 13:11:33 +0100

    fixed semicolon mistake

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var alexa = require('alexa-app');
 var fetch = require('node-fetch');
-var datefns = require('date-fns'):
+var datefns = require('date-fns');
 
 var PORT = process.env.PORT || 5000;
 var app = express();
@@ -30,8 +30,11 @@ alexaApp.intent('LaunchIntent', null, function(request, response) {
   return fetch('https://api.spacexdata.com/v2/launches/upcoming')
     .then(res => res.json())
     .then(json => {
-        var firstLaunch = json[0];
-      var next = datefns.distanceInWords(firstLaunch.launch_date_utc, new Date());
+      var firstLaunch = json[0];
+      var next = datefns.distanceInWords(
+        firstLaunch.launch_date_utc,
+        new Date()
+      );
 
       console.log('Response: ' + next);
       response.say('The next launch will be in' + next);
