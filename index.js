b7ce533755244c636386adfb3094fe1e16f780f7
David <david.joech@gmail.com>
2018-02-23 13:10:30 +0100

    return next launch date

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var alexa = require('alexa-app');
 var fetch = require('node-fetch');
+var datefns = require('date-fns'):
 
 var PORT = process.env.PORT || 5000;
 var app = express();
@@ -29,8 +30,11 @@ alexaApp.intent('LaunchIntent', null, function(request, response) {
   return fetch('https://api.spacexdata.com/v2/launches/upcoming')
     .then(res => res.json())
     .then(json => {
-      console.log('Response: ' + json[0].rocket.rocket_name + ' a' + response);
-      response.say(json[0].rocket.rocket_name);
+        var firstLaunch = json[0];
+      var next = datefns.distanceInWords(firstLaunch.launch_date_utc, new Date());
+
+      console.log('Response: ' + next);
+      response.say('The next launch will be in' + next);
       response.send();
     });
 });
