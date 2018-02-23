9ccd858ede79742a89f409573e12247601a4201d
David <david.joech@gmail.com>
2018-02-23 12:51:14 +0100

    test launch response

--- index.js
+++ index.js
@@ -1,26 +1,17 @@
 var express = require('express');
 var alexa = require('alexa-app');
+var fetch = require('node-fetch');
 
 var PORT = process.env.PORT || 5000;
 var app = express();
 
-// ALWAYS setup the alexa app and attach it to express before anything else.
-var alexaApp = new alexa.app('test');
+var alexaApp = new alexa.app('alexa');
 
 alexaApp.express({
   expressApp: app,
-
-  // verifies requests come from amazon alexa. Must be enabled for production.
-  // You can disable this if you're running a dev environment and want to POST
-  // things to test behavior. enabled by default.
-  checkCert: false,
-
-  // sets up a GET route when set to true. This is handy for testing in
-  // development, but not recommended for production. disabled by default
-  debug: true
+  checkCert: true,
+  debug: false
 });
-
-// now POST calls to /test in express will be handled by the app.request() function
 
 // from here on you can setup any other express routes or middlewares as normal
 
@@ -28,12 +19,18 @@ alexaApp.launch(function(request, response) {
   response.say('You launched the app!');
 });
 
+/*
 alexaApp.dictionary = {
   names: ['matt', 'joe', 'bob', 'bill', 'mary', 'jane', 'dawn']
 };
+*/
 
 alexaApp.intent('LaunchIntent', null, function(request, response) {
-  response.say('Success!');
+  fetch('https://api.spacexdata.com/v2/launches/upcoming')
+    .then(res => res.json())
+    .then(json => {
+      response.say(json[0].rocket.rocket_name);
+    });
 });
 
 app.listen(PORT, () =>
--- lambda.js
+++
@@ -1,9 +0,0 @@
-var alexa = require('alexa-app');
-
-var app = new alexa.app();
-app.launch(function(request, response) {
-  response.say('OK').send();
-});
-
-// connect to lambda
-exports.handler = app.lambda();
