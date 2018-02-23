eda11fa65d0a1768c2ead90d31f06aca45913b70
David <david.joech@gmail.com>
2018-02-23 12:08:15 +0100

    basic express server + alexa sdk setup

---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+// Initialize the Alexa SDK
+var Alexa = require('alexa-sdk');
+
+app.use(bodyParser.json());
+app.post('/', function(req, res) {
+    // Build the context manually, because Amazon Lambda is missing
+    var context = {
+        succeed: function (result) {
+            console.log(result);
+            res.json(result);
+        },
+        fail:function (error) {
+            console.log(error);
+        }
+    };
+    // Delegate the request to the Alexa SDK and the declared intent-handlers
+    var alexa = Alexa.handler(req.body, context);
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+});
