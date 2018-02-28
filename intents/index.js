c0acc4f3441cd2a7407c76ab501516399fc38a3f
David <david.joech@gmail.com>
2018-02-28 21:01:11 +0100

    extra function

--- intents/index.js
+++ intents/index.js
@@ -2,5 +2,7 @@ var LaunchIntent = require('./Launch');
 
 module.exports = function(app) {
   console.log('Register Intents');
-  app.intent('LaunchIntent', null, LaunchIntent);
+  app.intent('LaunchIntent', null, function(req, res) {
+    LaunchIntent(req, res);
+  });
 };
